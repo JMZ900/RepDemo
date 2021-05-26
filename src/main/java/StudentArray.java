ec3e1dfbaff7eb8ccf1df6d996dc58c8f8ab94cf
Jake M <Jake M@DESKTOP-5LE6UCM>
2021-05-26 12:25:46 -0400

    First commit

---
+++ src/main/java/StudentArray.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jake M
+ */
+public class StudentArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner in = new Scanner(System.in);
+        Student[] studList = new Student[5];
+        
+        for (int i =0; i<studList.length; i++) {
+            studList[i] = new Student();
+            System.out.println("Enter name: ");
+            String name = in.nextLine();
+            studList[i].setName(name);   
+        }
+        for (int i = 0; i<studList.length; i++) {
+            System.out.println(studList[i].getName());
+        }
+    }
+    
+}
